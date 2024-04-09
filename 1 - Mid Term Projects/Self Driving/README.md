7cf86b5a951ff044d1e4c01b329fdcd2c7ebbbb1
issaiass <issaiass@hotmail.com>
2024-04-09 18:44:16 -0500

    added mid term projects

---
+++ 1 - Mid Term Projects/Self Driving/README.md
@@ -0,0 +1,5 @@
+## Behavioral Cloning
+
+#### El simulador puede descargarse de aquí
+
+https://github.com/udacity/self-driving-car-sim
---
+++ 1 - Mid Term Projects/Self Driving/drive.py
@@ -0,0 +1,85 @@
+import socketio
+import eventlet
+import numpy as np
+from flask import Flask
+from tensorflow.keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
+
+sio = socketio.Server()
+ 
+app = Flask(__name__) #'__main__'
+
+
+class SimplePIController:
+    def __init__(self, Kp, Ki):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.set_point = 0.
+        self.error = 0.
+        self.integral = 0.
+
+    def set_desired(self, desired):
+        self.set_point = desired
+
+    def update(self, measurement):
+        # proportional error
+        self.error = self.set_point - measurement
+
+        # integral error
+        self.integral += self.error
+
+        return self.Kp * self.error + self.Ki * self.integral
+
+
+# TRACK 1
+# KP = 0.02, KI=0.0001, SPEED=15
+# KP = 0.025, KI=0.00001, SPEED=20
+
+# TRACK 2
+controller = SimplePIController(0.03, 0.01)
+speed_limit = 10
+controller.set_desired(speed_limit)
+
+def img_preprocess(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = img_preprocess(image)
+    image = np.array([image])
+    steering_angle = float(model.predict(image))
+    throttle = data["throttle"]
+    throttle = controller.update(float(speed))
+    print('{} {} {}'.format(steering_angle, throttle, speed))        
+    send_control(steering_angle, throttle)
+
+
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('Connected')
+    send_control(0, 0)
+ 
+def send_control(steering_angle, throttle):
+    sio.emit('steer', data = {
+        'steering_angle': steering_angle.__str__(),
+        'throttle': throttle.__str__()
+    })
+
+if __name__ == '__main__':
+    model = load_model('model.h5')
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
